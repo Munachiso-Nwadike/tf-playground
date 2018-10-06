18ccf1c3a047214d5839f9db6288f72d631a61ee
msn307 <22077758+Munachiso-Nwadike@users.noreply.github.com>
2018-10-06 16:41:07 +0100

    06.10.18 4:41pm

---
+++ exploring.py
@@ -0,0 +1,13 @@
+import tensorflow as tf 
+
+x = tf.Variable(3, name="x")
+y = tf.Variable(4, name="y")
+f = x*x*y + y + 2
+
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+ init.run() # actually initialize all the variables
+ result = f.eval()
+
+print(result)
+sess.close()
